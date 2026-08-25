aced3242af4f052a2d1412ea41d102d10ef86964
Mike V. <mike@cyway.it>
2026-08-25 23:14:47 +0200

    fixed double dashboard named route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::redirect('/', '/dashboard');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/trackables/', [DashboardController::class, 'index'])->name('trackables_index');
