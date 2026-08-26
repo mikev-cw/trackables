b036ec82b663679bd870599330201df7d56e8c75
Mike V. <mike@cyway.it>
2026-08-26 10:21:19 +0200

    feat: add custom pagination views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Trackable;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::defaultView('vendor.pagination.tabler');
+        Paginator::defaultSimpleView('vendor.pagination.simple-tabler');
+
         // Bind in routes the uid or alias param instead of the default.
         $this->app['router']->bind('trackable', function ($value) {
             return Trackable::where('uid', $value)
